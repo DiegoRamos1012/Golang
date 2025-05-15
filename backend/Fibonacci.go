ffdba62058e13675a77d83d47006c4affe320491
DiegoRamos1012 <diegorsantos09@Outlook.com>
2025-05-15 15:38:59 -0300

    Refactor Fibonacci implementation and update main to execute Fibonacci function

--- backend/Fibonacci.go
+++ backend/Fibonacci.go
@@ -1,6 +1,6 @@
 package main
 
-import ("fmt" "errors")
+import ("fmt")
 
 func FibonacciIterativo (n int) []int { // Os parametros são (n int) e retorna um array de inteiros ([]int
 	if n <= 0 {
@@ -10,5 +10,17 @@ func FibonacciIterativo (n int) []int { // Os parametros são (n int) e retorna 
 		return []int{0}
 	}
 
-	sequencia := []int{0, 1}
+	sequencia := []int{0, 1} // Inicializa a sequência com os dois primeiros números
+
+	for i := 2; i < n; i++ {
+		proximo := sequencia[i-1] + sequencia[i-2]
+		sequencia = append(sequencia, proximo) // Adiciona o próximo número
+	}
+
+	return sequencia
+}
+
+func executarFibonacci() {
+	n := 10 // Quantidade de números na sequência
+	fmt.Println("Fibonacci (Iterativo): ", FibonacciIterativo(n))
 }
